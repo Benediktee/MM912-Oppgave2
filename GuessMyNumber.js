6229d802e69a42b74dc972502793d38f1b71ce2b
Benediktee <143594065+Benediktee@users.noreply.github.com>
2023-09-13 20:30:29 +0200

    Update GuessMyNumber.js

--- GuessMyNumber.js
+++ GuessMyNumber.js
@@ -1,22 +1,36 @@
-const MIN_NUMBER = 1;
-const MAX_NUMBER = 100;
+        const MIN_NUMBER = 1;
+        const MAX_NUMBER = 100;
+        let tries = 9;
 
-const randomNumber = guessRandomNumber(MIN_NUMBER, MAX_NUMBER);
+        const randomNumber = guessRandomNumber(MIN_NUMBER, MAX_NUMBER);
+        const output = document.getElementById("outputtext");
 
-function guessRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-}
+        function guessRandomNumber(min, max) {
+            return Math.floor(Math.random() * (max - min)) + min;
+        }
 
-function guessYourNumber() {
-    const number = parseInt(document.getElementById('guess').value);
+        function guessYourNumber() {
+            const number = parseInt(document.getElementById('guess').value);
+            const li = document.createElement("li");
 
-    if (number === randomNumber) {
-        document.getElementById('message').textContent = 'Riktig!';
-        document.getElementById('ifYouWon').textContent = 'Takk for at du spilte!:)';
+            if (number === randomNumber) {
+                li.innerHTML = 'Riktig!';
 
-    } else if (number < randomNumber) {
-        document.getElementById('message').textContent = `For lavt! Du gjettet ${number}.`;
-    } else {
-        document.getElementById('message').textContent = `For høyt! Du gjettet ${number}.`;
-    }
-}
+            } else if (tries === 0) {
+                li.innerHTML = 'Du har ikke flere forsøk igjen..';
+
+            } else if (number < randomNumber) {
+                li.innerHTML = `For lavt! Du gjettet ${number}. Du har ${tries} forsøk igjen.`;
+                tries--;
+
+            } else {
+                li.innerHTML = `For høyt! Du gjettet ${number}. Du har ${tries} forsøk igjen.`;
+                tries--;
+            }
+
+            output.appendChild(li);
+        }
+
+        function spilleIgjen() {
+            location.reload()
+        }
